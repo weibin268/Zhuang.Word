cac02a53e67408ae152c88ece431112697817dfa
zhuangweibin <binweizhuang@hotmail.com>
2016-05-28 02:19:53 +0800

    aa

--- Zhuang.Word/Zhuang.Word.Test/AsposeWords.cs
+++ Zhuang.Word/Zhuang.Word.Test/AsposeWords.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aspose.Words;
+using Zhuang.Word.AsposeWords;
 
 namespace Zhuang.Word.Test
 {
@@ -9,6 +11,12 @@ namespace Zhuang.Word.Test
         [TestMethod]
         public void TestInsertDocumentAtBookmark()
         {
+            Document docA = new Document(@".\Files\a.docx");
+            Document docB = new Document(@".\Files\b.docx");
+
+            docA.InsertDocumentAtBookmark("bookmark1", docB);
+
+            docA.Save(@".\c.docx");
 
         }
     }
