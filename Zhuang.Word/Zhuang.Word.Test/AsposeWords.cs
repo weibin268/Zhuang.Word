099077e2c6e120982f3d3872fe49b0f5ca0488c1
zhuangweibin <binweizhuang@hotmail.com>
2016-05-27 14:21:29 +0800

    aa

---
+++ Zhuang.Word/Zhuang.Word.Test/AsposeWords.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zhuang.Word.Test
+{
+    [TestClass]
+    public class AsposeWords
+    {
+        [TestMethod]
+        public void TestInsertDocumentAtBookmark()
+        {
+
+        }
+    }
+}
