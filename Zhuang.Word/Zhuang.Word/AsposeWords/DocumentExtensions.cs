29c1f15b323b11499f4e1df117b05c1cfc46cc31
zhuangweibin <binweizhuang@hotmail.com>
2016-05-27 14:04:14 +0800

    aa

--- Zhuang.Word/Zhuang.Word/AsposeWords/DocumentExtensions.cs
+++ Zhuang.Word/Zhuang.Word/AsposeWords/DocumentExtensions.cs
@@ -5,19 +5,26 @@ using System.Text;
 using Aspose.Words;
 using System.IO;
 
-namespace Zhuang.Word
+namespace Zhuang.Word.AsposeWords
 {
     public static class DocumentExtensions
     {
         public static void AppendDocument(this Document doc, Document srcDoc)
         {
-            doc.FirstSection.PageSetup.SectionStart = Aspose.Words.SectionStart.Continuous;
+            doc.FirstSection.PageSetup.SectionStart = SectionStart.Continuous;
             doc.AppendDocument(srcDoc, ImportFormatMode.KeepSourceFormatting);
         }
 
         public static void Replace(this Document doc, string oldValue, string newValue)
         {
             doc.Range.Replace(oldValue, newValue, true, false);
+        }
+
+        public static void InsertDocumentAtBookmark(this Document doc, Document srcDoc)
+        {
+            //ExStart:InsertDocumentAtBookmark         
+            Bookmark bookmark = doc.Range.Bookmarks["insertionPlace"];
+            DocumentUtility.InsertDocument(bookmark.BookmarkStart.ParentNode, srcDoc);
         }
     }
 }
---
+++ Zhuang.Word/Zhuang.Word/AsposeWords/DocumentUtility.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Aspose.Words;
+
+namespace Zhuang.Word.AsposeWords
+{
+    class DocumentUtility
+    {
+        public static void InsertDocument(Node insertAfterNode, Document srcDoc)
+        {
+            // Make sure that the node is either a paragraph or table.
+            if ((!insertAfterNode.NodeType.Equals(NodeType.Paragraph)) &
+              (!insertAfterNode.NodeType.Equals(NodeType.Table)))
+                throw new ArgumentException("The destination node should be either a paragraph or table.");
+
+            // We will be inserting into the parent of the destination paragraph.
+            CompositeNode dstStory = insertAfterNode.ParentNode;
+
+            // This object will be translating styles and lists during the import.
+            NodeImporter importer = new NodeImporter(srcDoc, insertAfterNode.Document, ImportFormatMode.KeepSourceFormatting);
+
+            // Loop through all sections in the source document.
+            foreach (Section srcSection in srcDoc.Sections)
+            {
+                // Loop through all block level nodes (paragraphs and tables) in the body of the section.
+                foreach (Node srcNode in srcSection.Body)
+                {
+                    // Let's skip the node if it is a last empty paragraph in a section.
+                    if (srcNode.NodeType.Equals(NodeType.Paragraph))
+                    {
+                        Paragraph para = (Paragraph)srcNode;
+                        if (para.IsEndOfSection && !para.HasChildNodes)
+                            continue;
+                    }
+
+                    // This creates a clone of the node, suitable for insertion into the destination document.
+                    Node newNode = importer.ImportNode(srcNode, true);
+
+                    // Insert new node after the reference node.
+                    dstStory.InsertAfter(newNode, insertAfterNode);
+                    insertAfterNode = newNode;
+                }
+            }
+        }
+    }
+}
