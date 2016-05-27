363f1af04f2bf037a70e6e7378c86af5a5a60a32
zhuangweibin <binweizhuang@hotmail.com>
2016-05-27 11:28:01 +0800

    aa

---
+++ Zhuang.Word/Zhuang.Word/DocumentExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Aspose.Words;
+
+namespace Zhuang.Word
+{
+    public static class DocumentExtensions
+    {
+        public static void AppendDocument(this Document doc, Document srcDoc)
+        {
+            doc.AppendDocument(srcDoc, ImportFormatMode.KeepSourceFormatting);
+        }
+
+        public static void Replace(this Document doc, string oldValue, string newValue)
+        {
+            doc.Range.Replace(oldValue, newValue, true, false);
+        }
+    }
+}
